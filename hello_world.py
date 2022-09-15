4a3ee36be8ab9333c343d7428faf12968b4f6930
Cameron diedrich <cameron.diedrich@mths.ca>
2022-09-15 15:16:22 +0000

    init

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Cameron Diedrich
+# Created on: Sept 2022
+# This is the "Hello, World!" program, with proper style
+
+def main():
+    # This function prints to the console
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
+
